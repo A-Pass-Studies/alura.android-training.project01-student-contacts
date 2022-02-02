d117e8b440ac039ae65e641846d03fc6ac92d5d7
Anderson Passos Silva <anders.ps02@gmail.com>
2022-02-02 04:38:36 -0300

    Adiciona o primeiro ListView com Adapter

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -3,7 +3,12 @@ package br.com.alura.agenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ArrayList<String> alunos = new ArrayList<>(Arrays.asList("Anderson", "Laila", "Dina", "Maria", "Marcos"));
+
         setContentView(R.layout.activity_main);
+
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+
+        listaDeAlunos.setAdapter(
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
 
     }
 }
