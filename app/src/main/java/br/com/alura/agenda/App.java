7d38d6e7b0e5cb072a67d602caa9c3395744f7c8
Anderson Passos Silva <anders.ps02@gmail.com>
2022-02-19 10:45:03 -0300

    Adiciona edição de aluno

---
+++ app/src/main/java/br/com/alura/agenda/App.java
@@ -0,0 +1,26 @@
+package br.com.alura.agenda;
+
+import android.app.Application;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.dao.AlunoDAOImpl;
+import br.com.alura.agenda.model.Aluno;
+
+public class App extends Application {
+
+    private AlunoDAO alunoDAO = new AlunoDAOImpl();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mockAlunos();
+    }
+
+    private void mockAlunos() {
+        alunoDAO.salva(new Aluno("Jão", "1234455678", "joao@email.com"));
+        alunoDAO.salva(new Aluno("Maria", "1234455678", "joao@email.com"));
+        alunoDAO.salva(new Aluno("Jose", "1234455678", "joao@email.com"));
+        alunoDAO.salva(new Aluno("mateus", "1234455678", "joao@email.com"));
+    }
+}
--- app/src/main/java/br/com/alura/agenda/dao/AlunoDAOImpl.java
+++ app/src/main/java/br/com/alura/agenda/dao/AlunoDAOImpl.java
@@ -8,10 +8,22 @@ import br.com.alura.agenda.model.Aluno;
 public class AlunoDAOImpl implements AlunoDAO {
 
     private static List<Aluno> alunos = new ArrayList<>();
+    private static Integer contatorDeIds = 1;
 
     @Override
     public void salva(Aluno aluno) {
+        aluno.setId(contatorDeIds);
         alunos.add(aluno);
+        contatorDeIds++;
+    }
+
+    @Override
+    public void edita(Aluno aluno) {
+        for (Aluno alunoSalvo: alunos) {
+            if(alunoSalvo.getId() == aluno.getId()) {
+                alunos.set(alunos.indexOf(alunoSalvo), aluno);
+            }
+        }
     }
 
     @Override
